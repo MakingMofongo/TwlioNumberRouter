f85ebade77ad58a34b314f5ffc45dc61ef1a27ea
Abdul Rasheed Esa <rasheed3086@gmail.com>
2024-08-15 19:40:38 +0530

    fixed port variable

--- change_existing_url.js
+++ change_existing_url.js
@@ -2,7 +2,7 @@ const express = require("express");
 const twilio = require("twilio");
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 8080
 
 // dotenv
 require("dotenv").config();
